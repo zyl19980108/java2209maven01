6d50f7d8723df2ae50285a7522f3646d4cb415f0
zhaoyuliu <1512918722@qq.com>
2023-02-17 15:36:26 +0800

    third commit

--- src/main/java/git/gitTest.java
+++ src/main/java/git/gitTest.java
@@ -4,6 +4,7 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hello git3");
 
     }
 }
